b80ab1b8a6e12d81a5230c8094d9b27d1da4a475
hemant069 <hemantprajapati20500@gmail.com>
2025-11-06 15:45:58 +0530

    checked with db working fine

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,16 @@
 import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-
+import { api } from "@/convex/_generated/api";
 export default function Index() {
 
   const { toggleDarkMode } = useTheme();
+
+  const todos = useQuery(api.todos.getTodos);
+  const addTodo = useMutation(api.todos.addTodos);
+  const clearAllTodo = useMutation(api.todos.deleteAlltodos);
+  console.log(todos)
+
   return (
     <View
       style={styles.container}
@@ -12,7 +19,13 @@ export default function Index() {
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Toggle Dark Mode</Text>
       </TouchableOpacity>
-    </View>
+      <TouchableOpacity onPress={() => addTodo({ text: "first todo" })}>
+        <Text>Add todo</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => clearAllTodo()}>
+        <Text>Clear All todo</Text>
+      </TouchableOpacity>
+    </View >
   );
 }
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@/hooks/useTheme";
 import { Stack } from "expo-router";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 
+
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
   unsavedChangesWarning: false,
 });
